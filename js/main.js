0aa221f9a521f93cfa8d13368d4ea909709a6159
Skuli <skuliarnas@gmail.com>
2016-01-17 15:30:02 +0000

    added buttons with tools

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
 	console.log("I'm ready");
 
-
+	//used to test if jquery was working
 	$("#myCanvas").click(function(){
 		console.log("Click");
+	});
+
+	//funtion that determines what tool the user wants to use
+	$(".btn").click(function(){
+		var buttonID = $(this).attr('id');
+		buttonID = "#" + buttonID;
+		
+		$("#buttonMenu li button").each(function(){
+			$(this).removeClass();
+			$(this).addClass('btn btn-default');
+		})
+		$(buttonID).addClass("btn btn-success");
 	});
 })
