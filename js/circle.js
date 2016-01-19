a7bce26c0477f43246f0c1ce7c92d20fdb7c0be8
Skuli <skuliarnas@gmail.com>
2016-01-19 16:31:56 +0000

    circle works

--- js/circle.js
+++ js/circle.js
@@ -1,15 +1,20 @@
 var Circle = Shape.extend({
-	constructor: function(x, y){
-		this.base(x,y);
+	constructor: function(x, y, color, width){
+		this.base(x, y, color, width);
+		this.radius = 0;
 	},
 	draw: function(context, e){
+		context.strokeStyle = this.color;
+		context.lineWidth = this.width;
 		context.beginPath();
-		context.arc(this.x, this.y, 10, 0, 2 * Math.PI, false);
+		context.arc(this.x, this.y, this.radius * 8, 0, 2 * Math.PI, false);
 		context.stroke();
+		context.closePath();
 	},
 	drawing: function(canvas, e){
 		var rect = canvas.getBoundingClientRect();
-		this.endX = e.x - this.x - rect.left;
-		this.endY = e.y - this.y - rect.top;
+		this.endX = e.x - rect.left;
+		this.endY = e.y - rect.top;
+		this.radius = Math.sqrt(((this.endX - this.x)^2) + ((this.endY-this.y)^2));
 	}
 });
--- js/rectangle.js
+++ js/rectangle.js
@@ -1,9 +1,15 @@
 var Rectangle = Shape.extend({
-	constructor: function(x, y){
-		this.base(x,y);
+	constructor: function(x, y, color, width){
+		this.base(x,y, color, width);
 	},
 	draw: function(context, e){
+		if(this.x === this.endX && this.y === this.endY){
+			return;
+		}
+		context.strokeStyle = this.color;
+		context.lineWidth = this.width;
 		context.strokeRect(this.x, this.y, this.endX, this.endY);
+		context.closePath();
 	},
 	drawing: function(canvas, e){
 		var rect = canvas.getBoundingClientRect();
