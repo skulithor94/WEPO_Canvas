29087a127e1911099164d715b5b23a89670fb78b
Vilhjalmur Alex <vilhjalmuralex@gmail.com>
2016-01-20 17:04:38 +0000

    line thingy, still needs fixing

--- js/pen.js
+++ js/pen.js
@@ -1,19 +1,21 @@
 var Pen = Shape.extend({
-	constructor: function(x,y){
-		this.base(x,y);
+	constructor: function(x,y, color, width){
+		this.base(x,y, color, width);
 	},
 	draw: function(context, e){
-		this.x = this.endX;
-		this.y = this.endY;
 
-		
-
+		context.strokeStyle = this.color;
+		context.lineWidth = this.width;
+		context.lineCap = "round";
 		context.beginPath();
 		context.moveTo(this.x, this.y);
 		context.lineTo(this.endX, this.endY);
 		context.stroke();
 	},
 	drawing: function(canvas, e){
+		this.x = this.endX;
+		this.y = this.endY;
+
 		var rect = canvas.getBoundingClientRect();
 		this.endX = e.x - rect.left;
 		this.endY = e.y - rect.top;
